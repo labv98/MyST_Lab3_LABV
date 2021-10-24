3a8d0741f8a06c71ef8b518b88ef08790b122862
labv98 <lizale_barr@hotmail.com>
2021-10-24 14:49:22 -0500

    main_completed

--- main.py
+++ main.py
@@ -1,48 +1,75 @@
 
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- project: Aquí se encuentra el main del laboratorio 3                                             -- #
 # -- script: main.py : python script with the main functionality                                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- author: labv98                                                                       -- #
 # -- license: GPL-3.0 License                                                                            -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- repository: https://github.com/labv98/MyST_Lab3_LABV.git                                           -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
-import pandas as pd
-import data as dt
+from functions import *
+from visualizations import *
 
-# -- TEST 1 : 
-# verify that the script is being read
-print(dt.dict_test)
+# Por si existe algún warning
+import warnings
+warnings.simplefilter("ignore")
 
-# -- TEST 2 :
-# verify that installed pandas module works correctly
-df_dict_test = pd.DataFrame(dt.dict_test, index=[0, 1])
-print(df_dict_test)
+# ---- PARTE 1 ESTADÍSTICA DESCRIPTIVA ----
 
-# -- TEST 3 :
-# verify you can use plotly and visualize plots in jupyter notebook
+mt5, df_deals, df_orders = f_initialize("C:\Program Files\FxPro_MetaTrader5\\terminal64.exe", 5400340, "B31gdeC6", "FxPro-MT5")
+print(mt5)
 
-import chart_studio.plotly as py   # various tools (jupyter offline print)
-import plotly.graph_objects as go  # plotting engine
+df_deals.head()
 
-# example data
-df = pd.DataFrame({'column_a': [1, 2, 3, 4, 5], 'column_b': [1, 2, 3, 4, 5]})
-# basic plotly plot
-data = [go.Bar(x=df['column_a'], y=df['column_b'])]
-# instruction to view it inside jupyter
-py.iplot(data, filename='jupyter-basic_bar')
-# (alternatively) instruction to view it in web app of plotly
-# py.plot(data)
+df_orders.head()
 
-# -- TEST 4 :
-# verify you can use plotly and visualize plots in web browser locally
+positions, orders, deals = f_leer_archivo("C:/Users/Cesar/PycharmProjects/MyST_Lab3_LABV/ReportHistory_Alejandra.xlsx")
 
-import plotly.io as pio            # to define input-output of plots
-pio.renderers.default = "browser"  # to render the plot locally in your default web browser
+print(positions)
 
-# basic plotly plot
-plot_data = go.Figure(go.Bar(x=df['column_a'], y=df['column_b']))
-# instruction to view it in specified render (in this case browser)
-plot_data.show()
+orders.head()
+
+deals.head()
+
+f_pip_size("CADJPY")
+
+f_columnas_tiempos(positions)
+
+f_columnas_pips(positions)
+
+print(f_estadisticas_ba(positions)['df_1_tabla'])
+
+print(f_estadisticas_ba(positions)['df_2_ranking'])
+
+# ---- PARTE 2  MÉTRICAS DE ATRIBUCIÓN AL DESEMPEÑO ----
+
+# Pasar la fecha a formato str Y,m,d (fuera de la función porque solo se convierte una vez y
+# la función se manda llamar posteriormente)
+positions['Time_1'] = [i.strftime('%Y-%m-%d') for i in positions['Time_1']]
+
+f_evolucion_capital(positions)
+
+f_estadisticas_mad(positions)
+
+# ---- PARTE 3  BEHAVIORAL FINANCE ----
+
+# Pasar la fecha a formato str Y,m,d, H,M,S (fuera de la función porque solo se convierte una vez y
+# la función se manda llamar posteriormente)
+positions['Time_2'] = [i.strftime("%Y/%m/%d, %H:%M:%S") for i in positions['Time_2']]
+
+dic, status_quo, aversion_per, total_sen = f_be_de(positions)
+print(dic)
+
+# Pasando a DataFrame lo final del dic con los resultados
+bf = pd.DataFrame.from_dict(dic["df"], orient='index')
+print(bf)
+
+# ---- PARTE 4  VISUALIZACIONES ----
+
+graf_1(positions)
+
+graf_2(positions)
+
+graf_3(positions)
